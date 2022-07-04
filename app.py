1ffee0520d1492d3ac521622d58fb6aada006de4
Jaival Patel <pateljaival05@gmail.com>
2022-07-04 13:05:44 -0400

    nft creation

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import re
+from socket import create_connection
 from flask import Flask, redirect, render_template, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField, StringField
@@ -6,10 +7,9 @@ from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 import sys
-from thirdweb import ThirdwebSDK
-from dotenv import load_dotenv
+from test_transaction import nft
 
-load_dotenv()
+
 rel_file_path = None
 
 
@@ -26,23 +26,38 @@ class UploadFileForm(FlaskForm):
     submit = SubmitField('Submit')
 
 
+
 @app.route('/', methods=['GET','POST'])
 @app.route('/home', methods=['GET','POST'])
 def home():
-    form = UploadFileForm()
-    if form.validate_on_submit():
-        file = form.file.data #Grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], secure_filename(file.filename))) # Then save the file
-        file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
-        name_of_nft = form.name.data 
-        nft_description = form.description.data
-        image_link = form.image_link.data
-        rel_file_path = image_link
-        print('-'*26)
-        print(rel_file_path)
-        print('-'*26)
+    result = None
+    try:
+        form = UploadFileForm()
+        if form.validate_on_submit():
+            file = form.file.data #Grab the file
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], secure_filename(file.filename))) # Then save the file
+            file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
+            name_of_nft = form.name.data 
+            nft_description = form.description.data
+            image_link = form.image_link.data
+            rel_file_path = image_link
+            creation_obj = nft.create(name_of_nft, nft_description, image_link)
+            
+            result = creation_obj
+            if creation_obj != True:
+                return redirect(url_for('fail'))
+            else:
+                print('-'*26)
+                print(rel_file_path)
+                print('-'*26)
+                return redirect(url_for('landing'))
+        return render_template('index.html', form=form)
+    except Exception:
+        print(Exception)
         return redirect(url_for('landing'))
-    return render_template('index.html', form=form)
+        
+        
+
 
 
 @app.route('/landing', methods=['GET', 'POST'])
--- test_transaction.py
+++ test_transaction.py
@@ -11,18 +11,18 @@ class NFT:
     network = os.environ.get('API_URL')
     sdk = ThirdwebSDK.from_private_key(PRIVATE_KEY, network)
 
-    def create(self):
+    def create(self, name_of_nft, nft_description, image_link):
         NFT_COLLECTION_ADDRESS = '0x1b4ca86C0e779F8A63088664857fe2C64fA11CaB'
         nft_collection = self.sdk.get_nft_collection(NFT_COLLECTION_ADDRESS)
         nft_collection.mint(NFTMetadataInput.from_json({
-            "name": "test2",
-            "description": "Path Test",
-            "image": "https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg" # Takes Weblinks
+            "name": str(name_of_nft),
+            "description": str(nft_description),
+            "image": str(image_link) # Takes Weblinks
         }))
-        print('Completed')
+        return True
         #print(nft_collection.balance())
 
 
 nft = NFT()
 
-print(nft.create())
+#nft.create()
