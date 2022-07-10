c75de82b7675609f29ead315a134eaef73e88d23
Jaival Patel <pateljaival05@gmail.com>
2022-07-09 22:54:42 -0400

    fetching all nfts

--- js/test/publish_test.js
+++ js/test/publish_test.js
@@ -1,4 +1,5 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { Contract } from "ethers";
 
 const sdk = new ThirdwebSDK("mumbai")
 const contract = sdk.getNFTCollection("0x1b4ca86C0e779F8A63088664857fe2C64fA11CaB")
@@ -22,7 +23,25 @@ class NFT {
     get description () {
         return this._description
     }
+
+    mint_one() {
+        const metadata = {
+            'name': this._name,
+            'description': this._description,
+            'image':'https://www.researchgate.net/profile/Pablo-Fernandez-10/publication/315458901/figure/fig12/AS:541071486996481@1506012903656/Tree-level-Feynman-diagrams-for-coherent-forward-scattering-of-neutrinos-on-any.png'
+        }
+        return metadata
+    }
+
+    async fetch_all_nfts () {
+        const nfts = await contract.getAll();
+        console.log(nfts)
+    }
 }
 
-const test = new NFT('0xec2a636B21E2935897DB0D779A66221A82B4fd02', 'hi', 'test desc')
-console.log(test.description)
+const test = new NFT('0x127a95027B5c7E1D807433837C9cDD7e6f336803', 'hi', 'test desc')
+/*const metadata = test.mint_one()
+const tx = await contract.mintTo(test.walletAddress, metadata)
+const receipt = tx.receipt;
+const tokenId = tx.id
+const nft_data = await tx.data();*/
